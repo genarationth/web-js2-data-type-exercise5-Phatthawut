14726e24445803e8092f5d85cc50203684b26bc6
Phatthawut <42006909+Phatthawut@users.noreply.github.com>
2024-05-13 14:14:54 +0700

    Create object author

---
+++ script.js
@@ -0,0 +1,13 @@
+const author = {
+    authorID: 1,
+    name: 'john',
+    address: {
+        house_no: '99',
+        street: '12',
+        conty: 'thailand',
+        postcode: '10250'
+    },
+    url : 'google.com'
+}
+
+console.log(author);
